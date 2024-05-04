567c5b76c04f3f8ac823deff658b80bbbf13ca88
Shivam <shivamshibu2003@gmail.com>
2024-05-04 20:27:22 +0530

    a

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+// HomeDesign.jsx
+import React, { useState } from 'react';
 import Chatbox from '../component/chatbox/chatbox.jsx';
 import Dropbox from '../component/dropbox/dropbox.jsx';
+import './home.css';
 
 const HomeDesign = () => {
   return (
     <div className="home-div">
-      <Dropbox />
-      <Chatbox />
+      <Dropbox  />
+      <Chatbox  />
     </div>
   );
 };
